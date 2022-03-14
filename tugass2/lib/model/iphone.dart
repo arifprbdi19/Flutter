2f8736e10dc85bc468f1c722f09048803db28141
arifprbdi19 <123190019@student.upnyk.ac.id>
2022-03-14 11:53:44 +0700

    Tugas2praktikum

---
+++ tugass2/lib/model/iphone.dart
@@ -0,0 +1,54 @@
+class Iphone {
+  String image;
+  String name;
+  int price;
+
+  Iphone({
+    required this.image,
+    required this.name,
+    required this.price,
+  });
+}
+
+var getData = [
+  Iphone(
+    image:
+    "https://p-id.ipricegroup.com/uploaded_2747d4723527cc113f1671f83fa08e65.jpg",
+    name: "IPHONE 13",
+    price: 14999000,
+  ),
+  Iphone(
+    image: "https://ibox.co.id/product/iphone-12-pro-max-ibox",
+    name: "Iphone 12 Pro Max",
+    price: 17799000,
+  ),
+  Iphone(
+    image: "https://ibox.co.id/product/iphone-12-pro-ibox",
+    name: "Iphone 12 Pro",
+    price: 14999000,
+  ),
+  Iphone(
+    image: "https://ibox.co.id/product/iphone-12-ibox",
+    name: "Iphone 12",
+    price: 14999000,
+  ),
+  Iphone(
+    image: "https://ibox.co.id/product/iphone-12-mini-ibox",
+    name: "Iphone 12 Mini",
+    price: 12999000,
+  ),Iphone(
+    image: "https://ibox.co.id/product/apple-iphone-11-pro-max-ibox-1",
+    name: "Iphone 11 Pro Max ",
+    price: 12999000,
+  ),
+  Iphone(
+    image: "https://ibox.co.id/product/apple-iphone-11-pro-ibox-1",
+    name: "Iphone 11 Pro",
+    price: 12999000,
+  ),
+  Iphone(
+    image: "https://ibox.co.id/product/apple-iphone-11-new-package",
+    name: "Iphone 11",
+    price: 12999000,
+  ),
+];
